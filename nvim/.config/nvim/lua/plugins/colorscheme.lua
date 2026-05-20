c493e842c8cacf26654186a9f887dafe37d3221c
TaQsiimUwU <abdallahkassemhassan@gmail.com>
2026-05-20 14:51:23 +0300

    changed some sh in the nvim configs

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    opts = {
+      transparent = true, -- Enable transparency
+      styles = {
+        sidebars = "transparent", -- Make Neo-tree / sidebar transparent
+        floats = "transparent", -- Make floating windows transparent
+      },
+    },
+  },
+}
