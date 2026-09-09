12a141d7f23d44725919b8e0353656c6c8c844d5
TaQsiimUwU <abdallahkassemhassan@gmail.com>
2026-09-09 23:31:15 +0300

    made the caps key the esc key

--- hypr/.config/hypr/custom/general.lua
+++ hypr/.config/hypr/custom/general.lua
@@ -39,7 +39,7 @@ hl.config({
 	},
 	input = {
 		kb_layout = "us,ara",
-		kb_options = "grp:win_space_toggle",
+		kb_options = "grp:win_space_toggle , caps:escape",
 		numlock_by_default = true,
 		repeat_delay = 250,
 		repeat_rate = 35,
